45d47e87463349d866a8c0edce34bca35fb27b8b
bikathi-martin <bikathimartin@outlook.com>
2025-05-05 22:08:29 +0300

    Complete logic to generate random string based on CLI args

--- rsg/src/lib.rs
+++ rsg/src/lib.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use rand::Rng;
 
 #[derive(Debug)]
 pub enum CharactersMode {
@@ -15,7 +16,7 @@ impl CharactersMode {
             2 => Ok(Self::Uppercase),
             3 => Ok(Self::Lowercase),
             4 => Ok(Self::Mixed),
-            _ => Err("Failed to parse charcater mode!"),
+            _ => Err("Failed to parse character mode!"),
         }
     }
 }
@@ -49,4 +50,33 @@ pub struct RandomizerOptions {
     pub total_number: usize,
     pub length_of_strings: usize,
     pub mode: CharactersMode, // 1 for numeric only, 2 for uppercase, 3 for lowercase, 4 for mixed
+}
+
+pub fn generate_mix(chars_mode: &CharactersMode) -> String {
+    let numerals: String = String::from("0123456789");
+    let lowercase: String = String::from("abcdefghijklmnopqrstuvwxyz");
+    let uppercase: String = String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    match chars_mode {
+        CharactersMode::Numeric  => numerals,
+        CharactersMode::Uppercase => uppercase,
+        CharactersMode::Lowercase => lowercase,
+        CharactersMode::Mixed => format!("{}{}{}", numerals, lowercase, uppercase),
+    }
+}
+
+pub fn generate_string(options: &RandomizerOptions) -> String {
+    let scope = generate_mix(&options.mode);
+    let mut result = String::new();
+    
+    for _ in 0..options.length_of_strings {
+        let secret_number = rand::rng().random_range(0..scope.len());
+        result.push_str(scope.get(secret_number..=secret_number).unwrap());
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    
 }
--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -1,11 +1,12 @@
-use rsg::{ CliOptions, RandomizerOptions };
+use rsg::*;
 use clap::Parser;
 
-fn main() {
-    let cli_options: RandomizerOptions = match CliOptions::parse().convert_into() {
-        Ok(cli_options) => { cli_options },
-        _ => panic!("Failed to parse provided options!"),
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli_options: RandomizerOptions = CliOptions::parse().convert_into()?;
+    
+    for _ in 0..cli_options.total_number {
+        println!("{}", generate_string(&cli_options));
     };
     
-    dbg!("options: {}", cli_options);
+    Ok(())
 }
