a699d8db5b42c67133b511394111a2f4497723be
bikathi-martin <bikathimartin@outlook.com>
2025-05-05 10:59:18 +0300

    Add impl for parsing characters mode and converting CLI flags

--- rsg/src/lib.rs
+++ rsg/src/lib.rs
@@ -8,6 +8,18 @@ pub enum CharactersMode {
     Mixed,
 }
 
+impl CharactersMode {
+    fn delegate_mode(char_mode: &i32) -> Result<Self, &'static str> {
+        match char_mode {
+            1 => Ok(Self::Numeric),
+            2 => Ok(Self::Uppercase),
+            3 => Ok(Self::Lowercase),
+            4 => Ok(Self::Mixed),
+            _ => Err("Failed to parse charcater mode!"),
+        }
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about)]
 pub struct CliOptions {
@@ -19,6 +31,17 @@ pub struct CliOptions {
     
     #[arg(short = 'm', long = "chars-mode")]
     pub chars_mode: i32, // 1 for numeric only, 2 for uppercase, 3 for lowercase, 4 for mixed
+}
+
+impl CliOptions {
+    pub fn convert_into(self: &Self) -> Result<RandomizerOptions, &str> {
+        let char_mode = CharactersMode::delegate_mode(&self.chars_mode)?;
+        Ok(RandomizerOptions {
+            total_number: self.total_strings,
+            length_of_strings: self.length_of_strings,
+            mode: char_mode,
+        })
+    }
 }
 
 #[derive(Debug)]
