33550277ab0356d1afcc4736df91687689964adc
bikathi-martin <bikathimartin@outlook.com>
2025-05-05 09:25:44 +0300

    Create basic structure for command line arguments

--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct RandomizerOptions {
+    #[arg(short = 'n', long = "number-of-strings")]
+    number_of_strings: usize,
+    
+    #[arg(short = 'l', long = "length-of-strings")]
+    length_of_strings: usize,
+    
+    #[arg(short = 'm', long = "chars-mode")]
+    chars_mode: String, // 1 for numeric only, 2, for uppercase, 3 for lowercase,4 mixed
+}
+
 fn main() {
-    println!("Hello, world!");
+    let randomizer_options: RandomizerOptions = RandomizerOptions::parse();
+    dbg!("options: {}", randomizer_options);
 }
