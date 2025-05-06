d559533b131d78a5d81c9556031b30c50f2bdbb7
bikathi-martin <bikathimartin@outlook.com>
2025-05-06 13:32:20 +0300

    Replace convertional for loop with functional for-each

--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -1,17 +1,16 @@
 use rsg::parser::*;
-use rsg::generator::generate_string;
+use rsg::crude_generator::generate_string;
 use clap::Parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli_options: RandomizerOptions = CliOptions::parse().convert_into()?;
     
-    for _ in 0..cli_options.total_number {
-        println!("{}", generate_string(&cli_options));
-    };
+    (0..cli_options.total_number).for_each(|_| println!("{}", generate_string(&cli_options)));
     
     Ok(())
     
     // TODO: write up unit tests in the lib folder
     // TODO: create a default mode
     // TODO: Get rid of the RandomizerOptions struct. Use only one struct
+    // TODO: Add mode where we can use dev/
 }
