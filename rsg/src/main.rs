7256f2f4e0d3ff5a5a301c44207ff9ce96c83753
bikathi-martin <bikathimartin@outlook.com>
2025-05-06 22:03:12 +0300

    Get rid of intermediate CliOptions struct

--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -2,15 +2,11 @@ use rsg::parser::*;
 use rsg::crude_generator::generate_string;
 use clap::Parser;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cli_options: RandomizerOptions = CliOptions::parse().convert_into()?;
+fn main() {
+    let cli_options: RandomizerOptions = RandomizerOptions::parse();
     
     (0..cli_options.total_number).for_each(|_| println!("{}", generate_string(&cli_options)));
     
-    Ok(())
-    
-    // TODO: write up unit tests in the lib folder
     // TODO: create a default mode
-    // TODO: Get rid of the RandomizerOptions struct. Use only one struct
     // TODO: Add mode where we can use dev/
 }
--- rsg/src/parser.rs
+++ rsg/src/parser.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
+use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum CharactersMode {
     Numeric,
     Uppercase,
@@ -8,79 +9,29 @@ pub enum CharactersMode {
     Mixed,
 }
 
-impl CharactersMode {
-    fn delegate_mode(char_mode: &i32) -> Result<Self, &'static str> {
-        match char_mode {
-            1 => Ok(Self::Numeric),
-            2 => Ok(Self::Uppercase),
-            3 => Ok(Self::Lowercase),
-            4 => Ok(Self::Mixed),
-            _ => Err("Failed to parse character mode!"),
+impl FromStr for CharactersMode {
+    type Err = &'static str;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "1" | "numeric"  => Ok(CharactersMode::Numeric),
+            "2" | "uppercase" => Ok(CharactersMode::Uppercase),
+            "3" | "lowercase" => Ok(CharactersMode::Lowercase),
+            "4" | "mixed" => Ok(CharactersMode::Mixed),
+            _ => Err("Invaid characters mode. Use 1 (numeric), 2 (uppercase), 3 (lowercase), 4, (mixed)")
         }
     }
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, PartialEq)]
 #[command(version, about)]
-pub struct CliOptions {
+pub struct RandomizerOptions {
     #[arg(short = 't', long = "total-random-strings")]
-    pub total_strings: usize,
+    pub total_number: usize,
     
     #[arg(short = 'l', long = "length-of-strings")]
     pub length_of_strings: usize,
     
     #[arg(short = 'm', long = "chars-mode")]
-    pub chars_mode: i32, // 1 for numeric only, 2 for uppercase, 3 for lowercase, 4 for mixed
-}
-
-impl CliOptions {
-    pub fn convert_into(self: &Self) -> Result<RandomizerOptions, &str> {
-        let char_mode = CharactersMode::delegate_mode(&self.chars_mode)?;
-        Ok(RandomizerOptions {
-            total_number: self.total_strings,
-            length_of_strings: self.length_of_strings,
-            mode: char_mode,
-        })
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct RandomizerOptions {
-    pub total_number: usize,
-    pub length_of_strings: usize,
     pub mode: CharactersMode, // 1 for numeric only, 2 for uppercase, 3 for lowercase, 4 for mixed
-}
-
-#[cfg(test)]
-mod test {
-    use crate::parser::{CharactersMode, RandomizerOptions};
-
-    use super::CliOptions;
-
-    #[test]
-    #[should_panic]
-    fn should_panic_on_conversion() {
-        match CliOptions::convert_into(&CliOptions { total_strings: 1, length_of_strings: 1, chars_mode: 10 }) {
-            Ok(_) => (),
-            Err(_) => panic!("converter panicked!"),
-        }
-    }
-    
-    #[test]
-    fn should_correctly_convert() {
-        let mut converted_matches = false;
-        
-        if let Ok(randomizer_opts) = CliOptions::convert_into(&CliOptions { total_strings: 1, length_of_strings: 1, chars_mode: 4 }) {
-            if randomizer_opts == (RandomizerOptions {
-                total_number: 1, 
-                length_of_strings: 1,
-                mode: CharactersMode::Mixed,
-                
-            }) {
-                converted_matches = true
-            }
-        }
-        
-        assert_eq!(converted_matches, true);
-    }
 }
