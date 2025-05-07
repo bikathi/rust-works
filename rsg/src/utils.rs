613b81cd2d6ee601784868c22379550ef68fff93
bikathi-martin <bikathimartin@outlook.com>
2025-05-07 10:07:39 +0300

    Update method signature to return Vec<char>

--- rsg/src/utils.rs
+++ rsg/src/utils.rs
@@ -1,17 +1,17 @@
 use crate::parser::CharactersMode;
 
-pub fn generate_mix(chars_mode: &CharactersMode) -> String {
+pub fn generate_mix(chars_mode: &CharactersMode) -> Vec<char> {
     let numerals: String = String::from("0123456789");
     let lowercase: String = String::from("abcdefghijklmnopqrstuvwxyz");
     let uppercase: String = String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     let symbols: String = String::from("!@#$%^&*()_+");
 
     match chars_mode {
-        CharactersMode::Numeric  => numerals,
-        CharactersMode::Uppercase => uppercase,
-        CharactersMode::Lowercase => lowercase,
-        CharactersMode::Mixed => format!("{}{}{}", numerals, lowercase, uppercase),
-        CharactersMode::Noisy => format!("{}{}{}{}", numerals, lowercase, uppercase, symbols),
+        CharactersMode::Numeric  => numerals.chars().collect(),
+        CharactersMode::Uppercase => uppercase.chars().collect(),
+        CharactersMode::Lowercase => lowercase.chars().collect(),
+        CharactersMode::Mixed => format!("{}{}{}", numerals, lowercase, uppercase).chars().collect(),
+        CharactersMode::Noisy => format!("{}{}{}{}", numerals, lowercase, uppercase, symbols).chars().collect(),
         
     }
 }
