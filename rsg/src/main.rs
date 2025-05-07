57ae59585787af04a2349eed3969803da89c6d71
bikathi-martin <bikathimartin@outlook.com>
2025-05-07 10:11:24 +0300

    Improve logic for generating noisy strings from dev/urandom

--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -1,13 +1,25 @@
+use std::io::Error;
+
 use rsg::parser::*;
-use rsg::crude_generator::generate_string;
+use rsg::crude_generator::generate_crude_string;
+use rsg::urandom_generator::generate_urandom_string;
 use clap::Parser;
 
-fn main() {
-    let cli_options: RandomizerOptions = RandomizerOptions::parse();
+fn main() -> Result<(), Error> {
+    let randomizer_options: RandomizerOptions = RandomizerOptions::parse();
     
-    (0..cli_options.total_number).for_each(|_| println!("{}", generate_string(&cli_options)));
+    match &randomizer_options.mode {
+        CharactersMode::Uppercase | CharactersMode::Numeric | CharactersMode::Mixed | CharactersMode::Lowercase => {
+            (0..randomizer_options.total_number).for_each(|_| println!("{}", generate_crude_string(&randomizer_options)));
+        },
+        CharactersMode::Noisy => {
+            // TODO: The unwrap here potential flaw. Replace it with unwrap_
+            (0..randomizer_options.total_number).for_each(|_| println!("{}", generate_urandom_string(&randomizer_options).unwrap()));
+        },
+    }
     
-    // TODO: Add mode where we can use dev/
+    Ok(())
+    
     // TODO: Allow for writing output to a file
     // TODO: Provide better error handling and communication
 }
