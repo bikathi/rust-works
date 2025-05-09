3b6bc47b9d058672fe1f889cfbdc1247294c19e7
bikathi-martin <bikathimartin@outlook.com>
2025-05-09 09:25:12 +0300

    Add logic to allow writing random strings to a file

--- rsg/src/main.rs
+++ rsg/src/main.rs
@@ -3,23 +3,36 @@ use std::io::Error;
 use rsg::parser::*;
 use rsg::crude_generator::generate_crude_string;
 use rsg::urandom_generator::generate_urandom_string;
+use rsg::utils::{print_or_store, write_to_file};
 use clap::Parser;
 
-fn main() -> Result<(), Error> {
+fn main() {
     let randomizer_options: RandomizerOptions = RandomizerOptions::parse();
+    let mut string_buffer: Vec<String> = Vec::new();
     
-    match &randomizer_options.mode {
-        CharactersMode::Uppercase | CharactersMode::Numeric | CharactersMode::Mixed | CharactersMode::Lowercase => {
-            (0..randomizer_options.total_number).for_each(|_| println!("{}", generate_crude_string(&randomizer_options)));
-        },
-        CharactersMode::Noisy => {
-            // TODO: The unwrap here potential flaw. Replace it with unwrap_
-            (0..randomizer_options.total_number).for_each(|_| println!("{}", generate_urandom_string(&randomizer_options).unwrap()));
-        },
+    (0..randomizer_options.total_number).for_each(|_| {
+        match &randomizer_options.mode {
+            CharactersMode::Uppercase | CharactersMode::Numeric | CharactersMode::Mixed | CharactersMode::Lowercase => {
+                print_or_store(
+                    generate_crude_string(&randomizer_options), 
+                    &mut string_buffer,
+                    &randomizer_options.output_file
+                );
+            },
+            CharactersMode::Noisy => {
+                print_or_store(
+                    generate_urandom_string(&randomizer_options).unwrap(), 
+                    &mut string_buffer,
+                    &randomizer_options.output_file
+                );
+            },
+        }
+        
+    });
+    
+    if let Some(file) = &randomizer_options.output_file {
+        write_to_file(file, string_buffer)
     }
     
-    Ok(())
-    
-    // TODO: Allow for writing output to a file
     // TODO: Provide better error handling and communication
 }
--- rsg/src/utils.rs
+++ rsg/src/utils.rs
@@ -1,4 +1,6 @@
 use crate::parser::CharactersMode;
+use std::fs::File;
+use std::io::Write;
 
 pub fn generate_mix(chars_mode: &CharactersMode) -> Vec<char> {
     let numerals: String = String::from("0123456789");
@@ -14,4 +16,22 @@ pub fn generate_mix(chars_mode: &CharactersMode) -> Vec<char> {
         CharactersMode::Noisy => format!("{}{}{}{}", numerals, lowercase, uppercase, symbols).chars().collect(),
         
     }
+}
+
+pub fn print_or_store(random_str: String, buf: &mut Vec<String>, optional_output_file: &Option<String>) {
+    match &optional_output_file {
+        Some(_) => buf.push(random_str),
+        _ => println!("{}", random_str),
+    }
+}
+
+pub fn write_to_file(file: &String, buffer: Vec<String>) {
+    // join the buffer into one String with newlines as the separator
+    let string_to_write: String = buffer.join("\n");
+    
+    // create file and get it's handle
+    let mut output_file = File::create(file).expect("file creation failed!");
+    
+    // Write contents to the file
+    output_file.write(string_to_write.as_bytes()).expect("write to file failed!");
 }
